a2217477bc9d78b2af262d9d0518ba52a7fa9279
Amir-zeb <amirzeb695@gmail.com>
2026-07-29 16:48:13 +0500

    rock_paper_scissors - add types

--- rock-paper-scissors/rock_paper_scissors.py
+++ rock-paper-scissors/rock_paper_scissors.py
@@ -1,43 +1,45 @@
 import random
+from typing import Literal
 
-CHOICES = ("rock", "paper", "scissors")
-CHOICE_MAP = {"r": "rock", "p": "paper", "s": "scissors"}
-BEATS = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
+CHOICES:tuple[Literal["rock"], Literal["paper"], Literal["scissors"]] = ("rock", "paper", "scissors")
+CHOICE_MAP: dict[str, Literal["rock", "paper","scissors"]] = {"r": "rock", "p": "paper", "s": "scissors"}
+BEATS: dict[Literal["rock", "paper","scissors"], Literal["rock", "paper","scissors"]] = {"rock": "scissors", "paper": "rock", "scissors": "paper"}
 
-def get_user_choice()->str:
+def get_user_choice()->Literal["rock", "paper","scissors"]:
     while True:
-        input_str = input("Type your choice: ").strip().lower()
+        input_str:str = input("Type your choice: ").strip().lower()
         try:
             return CHOICE_MAP[input_str]
         except KeyError:
             print(f"'{input_str}' is invalid choice. Please try again")
 
-def get_computer_choice()->str:
-    num:int = random.randint(0, 2)
-    return CHOICES[num]
-    
+def get_computer_choice()->Literal["rock", "paper","scissors"]:
+    return random.choice(CHOICES)
 
-def decide_winner(user, computer) -> str:
+def decide_winner(
+    user: Literal["rock", "paper", "scissors"],
+    computer: Literal["rock", "paper", "scissors"]
+) -> Literal["win", "lose", "tie"]:
     print(f"You={user}, Computer={computer}")
     if user == computer:
         return "tie"
     return "win" if BEATS[user] == computer else "lose"
         
 
-def play_round()->str:
+def play_round()-> Literal["win", "lose", "tie"]:
     user=get_user_choice()
     computer=get_computer_choice()
     return decide_winner(user,computer)
 
-def main():
-    score = {"user": 0, "computer": 0}
+def main()->None:
+    score:dict[Literal["user","computer"],int] = {"user": 0, "computer": 0}
     
     print("ROCK__PAPER__SCISSORS")
     print("__WELCOME__")
     print(f"r={CHOICES[0]}, p={CHOICES[1]}, s={CHOICES[2]}")
     
     while True:
-        result =play_round()
+        result=play_round()
         print(f"result={result}")
         if result=='win':
             score["user"]+=1
